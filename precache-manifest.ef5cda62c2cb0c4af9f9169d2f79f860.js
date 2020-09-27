e34358f2f3168d167f7d48798ccbc7b86ccb2ab9
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-09-27 09:49:27 -0700

    Updates

---
+++ precache-manifest.ef5cda62c2cb0c4af9f9169d2f79f860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06b542c6caacba72d0b0ea2e4389821",
+    "url": "/frontend-assessment/index.html"
+  },
+  {
+    "revision": "b7a9ed4834ab42b14ecb",
+    "url": "/frontend-assessment/static/css/main.025872b0.chunk.css"
+  },
+  {
+    "revision": "3a59ff0531753a7c0d4c",
+    "url": "/frontend-assessment/static/js/2.5b96008e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-assessment/static/js/2.5b96008e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a9ed4834ab42b14ecb",
+    "url": "/frontend-assessment/static/js/main.74dd8f95.chunk.js"
+  },
+  {
+    "revision": "4bd4429c5e041a681854",
+    "url": "/frontend-assessment/static/js/runtime-main.e8b417df.js"
+  }
+]);
